5718ff3a925aaa7c7980edcfdb8d824d44f8a9c3
loi123 <loi65732102@gmail.com>
2024-06-09 10:28:55 +0700

    Update

--- src/main/java/com/example/demo/model/request/ProductRequest.java
+++ src/main/java/com/example/demo/model/request/ProductRequest.java
@@ -1,11 +1,15 @@
 package com.example.demo.model.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductRequest {
     private String name;
 
